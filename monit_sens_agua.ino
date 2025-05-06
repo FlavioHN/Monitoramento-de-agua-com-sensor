bac542ad857277178ae575430da9ccb382052104
Flavio Pinheiro <37315226+FlavioHN@users.noreply.github.com>
2025-05-06 15:37:04 -0300

    adicionado sensor numero 4

--- monit_sens_agua.ino
+++ monit_sens_agua.ino
@@ -1,10 +1,12 @@
 #define Sensor1 5 /* Define sensor1 como pino 5 */
 #define Sensor2 6 /* Define sensor2 como pino 6 */
-#define Sensor3 7 /* Define sensor2 como pino 7 */
+#define Sensor3 7 /* Define sensor3 como pino 7 */
+#define Sensor4 8 /* Define sensor2 como pino 8 */
 
 int sensor1 = 5; /* Alto */
-int sensor2 = 6; /* Meio */
-int sensor3 = 7; /* Baixo */
+int sensor2 = 6; /* Meio alto */
+int sensor3 = 7; /* Meio baixo */
+int sensor4 = 8; /* Baixo */
 
 /* int estado_inicial = 0; */
 
@@ -14,6 +16,7 @@ void setup() {
   pinMode(Sensor1, INPUT); /* Define o pino do sensor como entrada */
   pinMode(Sensor2, INPUT); /* Define o pino do sensor como entrada */
   pinMode(Sensor3, INPUT); /* Define o pino do sensor como entrada */
+  pinMode(Sensor4, INPUT); /* Define o pino do sensor como entrada */
 
 }
 
@@ -21,6 +24,7 @@ void loop() {
   int estado_sensor1 = digitalRead(sensor1);
   int estado_sensor2 = digitalRead(sensor2);
   int estado_sensor3 = digitalRead(sensor3);
+  int estado_sensor4 = digitalRead(sensor4);
 
   Serial.print("Estado do sensor 1: ");
   Serial.println(estado_sensor1);
@@ -31,21 +35,27 @@ void loop() {
   Serial.print("Estado do sensor 3: ");
   Serial.println(estado_sensor3);
   delay(1000);
+  Serial.print("Estado do sensor 4: ");
+  Serial.println(estado_sensor4);
+  delay(1000);
 
-  if ((estado_sensor1 == 1) && (estado_sensor2 == 1) && (estado_sensor3 == 1)) {
-    Serial.println("Reservatorio Cheio");
+  if ((estado_sensor1 == 1) && (estado_sensor2 == 1) && (estado_sensor3 == 1) && (estado_sensor4 == 1)) {
+    Serial.println("Reservatorio Cheio!");
   }
  
-  else if ((estado_sensor1 == 0) && (estado_sensor2 == 1) && (estado_sensor3 == 1)) {
-    Serial.println("Nivel de 100 a 70%");
+  else if ((estado_sensor1 == 0) && (estado_sensor2 == 1) && (estado_sensor3 == 1) && (estado_sensor4 == 1)) {
+    Serial.println("Nivel de 70 a 100%" - Meio cheio!");
   }
 
-  else if ((estado_sensor1 == 0) && (estado_sensor2 == 0) && (estado_sensor3 == 1)) {
-    Serial.println("Nivel de 70 a 35%");
+  else if ((estado_sensor1 == 0) && (estado_sensor2 == 0) && (estado_sensor3 == 1) && (estado_sensor4 == 1)) {
+    Serial.println("Nivel de 50% a 75% - Meio!");
   }
 
-  else if ((estado_sensor1 == 0) && (estado_sensor2 == 0) && (estado_sensor3 == 0)) {
-    Serial.println("Nivel abaixo de 35% - Critico!");
+  else if ((estado_sensor1 == 0) && (estado_sensor2 == 0) && (estado_sensor3 == 0) && (estado_sensor4 == 1)) {
+    Serial.println("Nivel em 25% e 50% - Meio baixo!");
+  }
+  else if ((estado_sensor1 == 0) && (estado_sensor2 == 0) && (estado_sensor3 == 0) && (estado_sensor4 == 0)) {
+    Serial.println("Nivel abaixo de 25% - Critico!");
   }
   else {
     Serial.println("ERRO - Verificar sensor!");
