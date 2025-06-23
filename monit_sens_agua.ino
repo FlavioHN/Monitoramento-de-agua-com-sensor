036b1445b35ae20a1a7ea26231fdb6de46cb3dad
Flavio Pinheiro <37315226+FlavioHN@users.noreply.github.com>
2025-06-22 22:59:13 -0300

    Corrigido numeração e posição dos sensores

--- monit_sens_agua.ino
+++ monit_sens_agua.ino
@@ -18,10 +18,10 @@ const char* servidor_url = "http://192.168.0.19:5000/receber";
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // Defini pinos dos sensores - válidos para ESP12N (ESP8266)
-const int sensor1Pin = 13; // Alto       -  GPIO12 - D7
-const int sensor2Pin = 12; // Medio alto -  GPIO13 - D6
-const int sensor3Pin = 14; // Meio baixo -  GPIO14 - D5
-const int sensor4Pin = 16; // Baixo      -  GPIO16 - D0
+const int sensor4Pin = 13; // Alto       -  GPIO12 - D7
+const int sensor3Pin = 12; // Medio alto -  GPIO13 - D6
+const int sensor2Pin = 14; // Meio baixo -  GPIO14 - D5
+const int sensor1Pin = 16; // Baixo      -  GPIO16 - D0
 
 // int estado_inicial = 0;
 String ultimo_nivel = "";
@@ -32,10 +32,10 @@ void setup() {
   // Serial.begin(9600);
 
   // Define pinos como entradas
-  pinMode(sensor1Pin, INPUT);
-  pinMode(sensor2Pin, INPUT);
-  pinMode(sensor3Pin, INPUT);
   pinMode(sensor4Pin, INPUT);
+  pinMode(sensor3Pin, INPUT);
+  pinMode(sensor2Pin, INPUT);
+  pinMode(sensor1Pin, INPUT);
 
   lcd.begin(16,2);
   lcd.init();
@@ -80,16 +80,16 @@ void setup() {
 }
 
 void loop() {
-  int estado_sensor1 = digitalRead(sensor1Pin);
-  int estado_sensor2 = digitalRead(sensor2Pin);
-  int estado_sensor3 = digitalRead(sensor3Pin);
   int estado_sensor4 = digitalRead(sensor4Pin);
+  int estado_sensor3 = digitalRead(sensor3Pin);
+  int estado_sensor2 = digitalRead(sensor2Pin);
+  int estado_sensor1 = digitalRead(sensor1Pin);
 
   // Serial.print("Estado do sensor 1: ");
   // Serial.println(estado_sensor1);
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Sensor 1:");
+  lcd.print("Sensor 1 - Baixo");
   lcd.setCursor(0,1);
   lcd.print("Estado: ");
   lcd.setCursor(11,1);
@@ -100,7 +100,7 @@ void loop() {
   // Serial.println(estado_sensor2);
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Sensor 2:");
+  lcd.print("Sensor 2 - Meio");
   lcd.setCursor(0,1);
   lcd.print("Estado: ");
   lcd.setCursor(11,1);
@@ -111,7 +111,7 @@ void loop() {
   // Serial.println(estado_sensor3);
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Sensor 3:");
+  lcd.print("Sensor 3 - MC");
   lcd.setCursor(0,1);
   lcd.print("Estado: ");
   lcd.setCursor(11,1);
@@ -122,7 +122,7 @@ void loop() {
   // Serial.println(estado_sensor4);
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Sensor 4:");
+  lcd.print("Sensor 4 - Cheio");
   lcd.setCursor(0,1);
   lcd.print("Estado: ");
   lcd.setCursor(11,1);
@@ -134,25 +134,25 @@ void loop() {
   String niveldeagua_lcd;
   int enviar_sensor;
 
-  if (estado_sensor1 == HIGH) {
+  if (estado_sensor4 == HIGH) {
     niveldeagua_monitor = "Reservatorio CHEIO!";
     niveldeagua_lcd = "CHEIO 100%!";
     enviar_sensor = 4;
   }
  
-  else if (estado_sensor2 == LOW) {
+  else if (estado_sensor3 == LOW) {
     niveldeagua_monitor = "Nivel em 75% - Meio cheio!";
     niveldeagua_lcd = "ENTRE 75-99%";
     enviar_sensor = 3;
   }
 
-  else if (estado_sensor3 == LOW) {
+  else if (estado_sensor2 == LOW) {
     niveldeagua_monitor = "Nivel em 50% - Meio!";
     niveldeagua_lcd = "ENTRE 50-74%";
     enviar_sensor = 2;
   }
 
-  else if (estado_sensor4 == HIGH) {
+  else if (estado_sensor1 == HIGH) {
     niveldeagua_monitor = "Nivel em 25% - Baixo!";
     niveldeagua_lcd = "ENTRE 25-49%";
     enviar_sensor = 1;
@@ -174,10 +174,10 @@ void loop() {
 
   // Exibir resultado via Monitor Serial
   Serial.println("---#---#---#---#---#--");
-  Serial.print("Estado do sensor 1: "); Serial.println(estado_sensor1);
-  Serial.print("Estado do sensor 2: "); Serial.println(estado_sensor2);
-  Serial.print("Estado do sensor 3: "); Serial.println(estado_sensor3);
   Serial.print("Estado do sensor 4: "); Serial.println(estado_sensor4);
+  Serial.print("Estado do sensor 3: "); Serial.println(estado_sensor3);
+  Serial.print("Estado do sensor 2: "); Serial.println(estado_sensor2);
+  Serial.print("Estado do sensor 1: "); Serial.println(estado_sensor1);
   Serial.println("----------------------");
   Serial.println("Nivel do reservatorio: " + niveldeagua_monitor);
   Serial.println("----------------------");
